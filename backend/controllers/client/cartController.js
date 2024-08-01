521eb5faad9f95d5f16694079ce8e257620553ee
sontransonn <sontransonn@gmail.com>
2024-08-01 19:57:33 +0700

    env

--- backend/controllers/client/cartController.js
+++ backend/controllers/client/cartController.js
@@ -1,3 +1,5 @@
+import { mongo } from "mongoose"
+
 import CARDS from "../../models/cardsModel.js"
 
 class cartController {
@@ -10,7 +12,7 @@ class cartController {
             const card_products = await CARDS.aggregate([{
                 $match: {
                     userId: {
-                        $eq: new ObjectId(userId)
+                        $eq: new mongo.ObjectId(userId)
                     }
                 }
             },
@@ -31,7 +33,70 @@ class cartController {
                 card_product_count = card_product_count + outOfStockProduct[i].quantity
             }
 
+            const stockProduct = card_products.filter(p => p.products[0].stock >= p.quantity)
+            for (let i = 0; i < stockProduct.length; i++) {
+                const {
+                    quantity
+                } = stockProduct[i]
+                card_product_count = card_product_count + quantity
+                buy_product_item = buy_product_item + quantity
+                const {
+                    price,
+                    discount
+                } = stockProduct[i].products[0]
+                if (discount !== 0) {
+                    calculatePrice = calculatePrice + quantity * (price - Math.floor((price * discount) / 100))
+                } else {
+                    calculatePrice = calculatePrice + quantity * price
+                }
+            }
 
+            let p = []
+            let unique = [...new Set(stockProduct.map(p => p.products[0].sellerId.toString()))]
+            for (let i = 0; i < unique.length; i++) {
+                let price = 0;
+                for (let j = 0; j < stockProduct.length; j++) {
+                    const tempProduct = stockProduct[j].products[0]
+                    if (unique[i] === tempProduct.sellerId.toString()) {
+                        let pri = 0;
+                        if (tempProduct.discount !== 0) {
+                            pri = tempProduct.price - Math.floor((tempProduct.price * tempProduct.discount) / 100)
+                        } else {
+                            pri = tempProduct.price
+                        }
+                        pri = pri - Math.floor((pri * co) / 100)
+                        price = price + pri * stockProduct[j].quantity
+                        p[i] = {
+                            sellerId: unique[i],
+                            shopName: tempProduct.shopName,
+                            price,
+                            products: p[i] ? [
+                                ...p[i].products,
+                                {
+                                    _id: stockProduct[j]._id,
+                                    quantity: stockProduct[j].quantity,
+                                    productInfo: tempProduct
+                                }
+                            ] : [{
+                                _id: stockProduct[j]._id,
+                                quantity: stockProduct[j].quantity,
+                                productInfo: tempProduct
+
+                            }]
+                        }
+                    }
+
+                }
+            }
+
+            return res.status(200).json({
+                card_products: p,
+                price: calculatePrice,
+                card_product_count,
+                shipping_fee: 85 * p.length,
+                outOfStockProduct,
+                buy_product_item
+            })
         } catch (error) {
             console.log(error);
             res.status(500).json({ error: error })
